607b832411b5a7830437ca0c3d1d0fe03f7b550e
Charles Desiderio <cjdesi@gmail.com>
2021-11-16 22:34:35 -0600

    Added shop frame styling

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -2,11 +2,13 @@ import styles from './Frame.module.css'
 
 import Draggable from 'react-draggable'
 
+
+
 const Frame = (props) => {
   if (props.drag && props.item.name === 'Dungeon Marker') return (
     <Draggable grid={[10, 10]}>
       <div className={styles.frame}>
-        <input defaultValue={`New Frame`} />
+        <input className={styles.titleInput} defaultValue={`New Dungeon`} />
         <div></div>
         <div></div>
         <div></div>
@@ -16,11 +18,20 @@ const Frame = (props) => {
 
   if (props.drag && props.item.name === 'Shop Marker') return (
     <Draggable grid={[10, 10]}>
-    <div className={styles.frame}>
-      <input defaultValue={`Shop Frame`} />
+    <div className={styles.rupeeFrame}>
+      <input className={styles.titleInput} defaultValue={`Shop Frame`} />
       <div></div>
       <div></div>
       <div></div>
+      <div className={styles.rupeeInputContainer}>
+        <input className={styles.frameShopInput} defaultValue={`0`} /><img src={`/icons/yellowrupee.png`} alt="Rupee" />
+      </div>
+      <div className={styles.rupeeInputContainer}>
+        <input className={styles.frameShopInput} defaultValue={`0`} /><img src={`/icons/yellowrupee.png`} alt="Rupee" />
+      </div>
+      <div className={styles.rupeeInputContainer}>
+        <input className={styles.frameShopInput} defaultValue={`0`} /><img src={`/icons/yellowrupee.png`} alt="Rupee" />
+      </div>
     </div>
   </Draggable>
   )
